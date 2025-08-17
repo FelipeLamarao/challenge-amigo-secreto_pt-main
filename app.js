a1b6318f72463ecee9dbd65920d20f705de0c050
felipe <obrfelipelamarao@gmail.com>
2025-08-16 21:50:17 -0400

    atualzação de uma função

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeParticipantes = [];
 
-function adicionarAmigo() {
+function adicionarNomes() {
     let nomeAmigo = document.getElementById("nomeAmigo").value;
 
     if (nomeAmigo && !listaDeParticipantes.includes(nomeAmigo)) {
